deb6c10e25271df8673003fcf6f51b2b58d663b5
Dien Nguyen <khanhdien@gmail.com>
2022-11-29 14:36:41 +0700

    delete meduverse folder

--- apps/meduverse/src/state/user/smartRouter.ts
+++
@@ -1,8 +0,0 @@
-import { useAtom } from 'jotai'
-import { atomWithStorage } from 'jotai/utils'
-
-const userSmartRouterAtom = atomWithStorage<boolean>('pcs:smartRouter', true)
-
-export function useUserSmartRouter() {
-  return useAtom(userSmartRouterAtom)
-}
